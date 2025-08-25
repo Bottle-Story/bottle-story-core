cff60a5ac4a08240fb94af6a1ab59f1e30822f28
kimyj <yongjjun8718@gmail.com>
2025-08-25 20:39:50 +0900

    날씨Api

--- src/main/java/com/kyj/fmk/core/util/KmaGrid.java
+++ src/main/java/com/kyj/fmk/core/util/KmaGrid.java
@@ -5,9 +5,9 @@ import com.kyj.fmk.core.model.wheather.KmaEntity;
 import static java.lang.Math.*;
 
 public class KmaGrid {
-         private static int[] latLonToGrid(double lat, double lon) {
+
+    private static int[] latLonToGrid(double lat, double lon) {
         double DEGRAD = Math.PI / 180.0;
-        double RADDEG = 180.0 / Math.PI;
 
         double re = 6371.00877; // 지구 반경(km)
         double grid = 5.0;      // 격자 간격(km)
@@ -15,18 +15,21 @@ public class KmaGrid {
         double slat2 = 60.0;
         double olon = 126.0;
         double olat = 38.0;
-        double xo = 43;         // 기준점 X좌표
-        double yo = 136;        // 기준점 Y좌표
+        double xo = 210 / grid; // TM 좌표 기준점 X
+        double yo = 675 / grid; // TM 좌표 기준점 Y
 
         double sn = tan(PI * 0.25 + slat2 * DEGRAD * 0.5) / tan(PI * 0.25 + slat1 * DEGRAD * 0.5);
         sn = log(cos(slat1 * DEGRAD) / cos(slat2 * DEGRAD)) / log(sn);
+
         double sf = tan(PI * 0.25 + slat1 * DEGRAD * 0.5);
         sf = pow(sf, sn) * cos(slat1 * DEGRAD) / sn;
+
         double ro = tan(PI * 0.25 + olat * DEGRAD * 0.5);
         ro = re * sf / pow(ro, sn);
 
         double ra = tan(PI * 0.25 + lat * DEGRAD * 0.5);
         ra = re * sf / pow(ra, sn);
+
         double theta = lon * DEGRAD - olon * DEGRAD;
         if (theta > PI) theta -= 2.0 * PI;
         if (theta < -PI) theta += 2.0 * PI;
@@ -38,14 +41,12 @@ public class KmaGrid {
         return new int[]{x, y};
     }
 
-    public static KmaEntity getKmaEntity(double lat, double lot){
-        int[] arr= latLonToGrid(lat,lot);
+    public static KmaEntity getKmaEntity(double lat, double lon) {
+        int[] arr = latLonToGrid(lat, lon);
 
         KmaEntity kmaEntity = new KmaEntity();
         kmaEntity.setNx(String.valueOf(arr[0]));
         kmaEntity.setNy(String.valueOf(arr[1]));
-        return  kmaEntity;
+        return kmaEntity;
     }
-
-
 }
